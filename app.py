7294c0f110a92b486656ccd3014172f397c1e04e
wonderNG <82185052+wonderNG@users.noreply.github.com>
2023-05-12 14:22:57 +0100

    Prise en Main avec Flask et Base de données Réussie

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+app.static_folder = 'static/'
+app.static_url_path = 'static/'
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
